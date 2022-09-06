ccedb2b3d651bca93a0c7db74392b70801a2b1c4
khubaib.umer <khubaib.umer@afiniti.com>
2022-09-07 04:09:18 +0500

    Cleanup

--- src/packet.c
+++ src/packet.c
@@ -70,38 +70,3 @@ packet_t* read_packet(FILE* file)
 	}
 	return NULL;
 }
-
-//packet_t* read_packet(FILE* file, size_t* offset)
-//{
-//	// In our input file after every 2 characters there is a space
-//	// We will try and read 5-bytes per read to accomodate 2char+1space+2char
-//	// fgets will only read 5 bytes of data to make space for null byte
-//	char tmp[6] = { 0 };
-//	uint8_t packet[2048] = { 0 };
-//	size_t count = 0;
-//	while (fgets(&tmp[0], sizeof tmp, file) != NULL)
-//	{
-//		if (strstr(tmp, "LK")) //BLK
-//		{
-//			continue;
-//		}
-//		else if (strstr(tmp, "COMP")) // COMPLETE
-//		{
-//			break;
-//		}
-//		else if (tmp[2] != ' ')
-//		{
-//			continue; // Data format is not inline with input assumption
-//		}
-//		unsigned long byte1 = strtoul(&tmp[0], NULL, 16);
-//		unsigned long byte2 = strtoul(&tmp[3], NULL, 16);
-//		packet[count++] = byte1;
-//		packet[count++] = byte2;
-//		printf("0x%02lx 0x%02lx\n", byte1, byte2);
-//	}
-//	packet_t* pk = calloc(1, sizeof(packet_t));
-//	pk->count = count;
-//	pk->data = calloc(count, sizeof(uint8_t));
-//	memcpy(pk->data, packet, count);
-//	return pk;
-//}
